38d6c3919260aff9082570f824a4b0df82c59d06
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-02-25 22:08:55 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('button clicked');
+
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === '') {
+    msg.innerHTML = 'Post cannot be blank';
+    console.log('failure');
+  } else {
+    console.log('successs');
+    msg.innerHTML = '';
+  }
+};
